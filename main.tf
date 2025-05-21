507a0742a9980986c0bf1d9a35ed47b7c966e99d
NKXK26 <nicolaskho1@gmail.com>
2025-05-21 21:09:03 +0800

    wd

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
-resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
- tags = {
-   Name = var.name_tag,
- }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "public_instance" {
+  ami           = "${data.aws_ami.latest_amazon_linux.id}" # Dynamically fetched AMI
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.name_tag
+  }
 }
